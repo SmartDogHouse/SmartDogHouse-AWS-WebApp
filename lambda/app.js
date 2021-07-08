8881d76e03f78af980c3a7d96907478553352da5
camillo2008 <jordancaminati@libero.it>
2021-07-08 13:34:27 -0700

    Renamed file to be more clear

--- lambda/app.js
+++ lambda/app.js
@@ -1,6 +1,7 @@
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
 
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -13,17 +14,15 @@
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  * 
  */
- exports.handler = async (event, context) => {
-  // ------------ NodeJS runtime ---------------
-  //--------------------------------------------
+ exports.getWaterConsumptionByDog = async (event, context) => {
+
+  const region = 'eu-west-2';
+  let queryManager = new QueryManager()
+  let dbManager = new DynamoDBManager(region)
 
   let statusCode = 200
-  // Create the DynamoDB Client with the region you want
-  const region = 'eu-west-2';
 
-  // Call DynamoDB's executeStatement API
-  let dbManager = new DynamoDBManager(region)
-  let result = await dbManager.executeExecuteStatement(query1());
+  let result = await dbManager.executeExecuteStatement(queryManager.getWaterConsumptionByDog("DOG#c02","22135489940","22135489945"));
   console.info('ExecuteStatement API call has been executed.')
 
   switch (result) {
@@ -39,21 +38,67 @@
   };
 
   return response
-
-
-  function query1() {
-      return {
-          "Statement": "SELECT daily_water_upper_bound, daily_water_lower_bound FROM dogs_logs WHERE contains(PK, 'DOG#c01')AND contains(SK, '#PROFILE#')"
-      }
-  }
-
-  function query2() {
-      return {
-          "Statement": "SELECT * FROM dogs_logs WHERE contains(PK, 'LOG#wcons') AND contains(SK, 'DOG#c02') AND (time_stamp BETWEEN '22135489941' AND '22135489943') AND val BETWEEN 99 AND 101"
-      }
-  }
-
 };
+
+exports.getFoodConsumptionByDog = async (event, context) => {
+
+    const region = 'eu-west-2';
+    let queryManager = new QueryManager()
+    let dbManager = new DynamoDBManager(region)
+  
+    let statusCode = 200
+  
+    let result = await dbManager.executeExecuteStatement(queryManager.getFoodConsumptionByDog("DOG#c02","22135489940","22135489945"));
+    console.info('ExecuteStatement API call has been executed.')
+  
+    switch (result) {
+        case null:
+        case "":
+            statusCode = 500;
+            break;
+    }
+  
+    const response = {
+        statusCode: statusCode,
+        body: result
+    };
+  
+    return response
+  };
+
+
+  exports.waterConsumptionAlarm = async (event, context) => {
+
+    const region = 'eu-west-2';
+    let queryManager = new QueryManager()
+    let dbManager = new DynamoDBManager(region)
+  
+    var today = new Date()
+    var tomorrow = new Date(today)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    
+    today = today.toISOString().slice(0, 19) 
+    tomorrow = tomorrow.toISOString().slice(0, 19) 
+    console.log(tomorrow)
+    try {
+        let dogs = await dbManager.executeExecuteStatement(queryManager.getDogsWithWaterRange());
+        dogs.array.forEach(element => {
+            let ranges = await dbManager.executeExecuteStatement(queryManager.getDogWaterRanges(element));
+            let values = await dbManager.executeExecuteStatement(queryManager.getWaterConsumptionByDog(element,today,tomorrow));
+            let sum = values.reduce((acc,newEl) => acc+newEl)
+            if(sum < ranges[0].daily_water_lower_bound ||  sum > ranges[0].daily_water_upper_bound ){
+                //TODO SEND WEBSITE NOTIFY
+            }
+        });
+        return "Success"
+    } catch (err) {
+        return "Error"
+    }
+
+
+  };
+
+
 
 class DynamoDBManager {
 
@@ -140,8 +185,76 @@ class DynamoDBManager {
                   `increase account level throughput before retrying. Error: ${err.message}`);
               return;
           default:
-              console.error(`Unexpected error encountered`);
+              console.error(`Unexpected error encountered::${err.code}`);
               return;
       }
   }
 }
+
+
+class QueryManager {
+    constructor() {}
+  
+    getDogWaterRanges(dog) {
+      return {
+          "Statement" : 
+          `SELECT daily_water_upper_bound, daily_water_lower_bound 
+          FROM dogs_logs 
+          WHERE contains(PK, 'DOG#${dog}')
+          AND contains(SK, '#PROFILE#')`
+        } 
+    }
+  
+    getDogsWithWaterRange() {
+        return {
+            "Statement" : 
+            `SELECT PK
+            FROM dogs_logs
+            WHERE contains(PK, 'DOG#') 
+            AND contains(SK, '#PROFILE#') 
+            AND daily_water_lower_bound>0 
+            AND daily_water_upper_bound<3000`
+          } 
+      }
+
+      getDogWaterRanges(dog) {
+        return {
+            "Statement" : 
+            `SELECT daily_water_upper_bound, daily_water_lower_bound 
+            FROM dogs_logs 
+            WHERE contains(PK, 'DOG#${dog}')
+            AND contains(SK, '#PROFILE#')`
+          } 
+      }
+
+
+    query2(daily_water_lower_bound,daily_water_upper_bound) {
+      return {
+          "Statement" :
+          "SELECT * FROM dogs_logs WHERE contains(PK, 'LOG#wcons') AND contains(SK, 'DOG#c02') AND (time_stamp BETWEEN '22135489941' AND '22135489943') AND val BETWEEN "+ daily_water_lower_bound +" AND "+ daily_water_upper_bound
+        } 
+    }
+  
+  
+    getWaterConsumptionByDog(dog,lowerTimeS,upperTimeS) {
+      return this.getConsumptionByDog("LOG#wcons",dog,lowerTimeS,upperTimeS)
+    }
+  
+    getConsumptionByDog(type,dog,lowerTimeS,upperTimeS) {
+      return {
+          "Statement" : 
+          `SELECT val 
+          FROM dogs_logs 
+          WHERE contains(PK, '"${type}"')
+          AND contains(SK, '"${dog}"') 
+          AND (time_stamp BETWEEN '"${lowerTimeS}"' 
+          AND '"${upperTimeS}"')`
+        } 
+    }
+  
+    getFoodConsumptionByDog(dog,lowerTimeS,upperTimeS) {
+      return this.getConsumptionByDog("LOG#fcons",dog,lowerTimeS,upperTimeS)
+    }
+  }
+    
+  
