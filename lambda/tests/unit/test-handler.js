1881eabf49fff4e5103dad5771b5c11647727a65
camillo2008 <jordancaminati@libero.it>
2021-07-26 15:22:57 +0200

    Setup first test

--- lambda/tests/unit/test-handler.js
+++ lambda/tests/unit/test-handler.js
@@ -7,16 +7,14 @@ var event, context;
 
 describe('Tests index', function () {
     it('verifies successful response', async () => {
-        const result = await app.lambdaHandler(event, context)
-
+        const result = await app.getDogs(event, context)
         expect(result).to.be.an('object');
         expect(result.statusCode).to.equal(200);
         expect(result.body).to.be.an('string');
-
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
+        let response = JSON.parse(result.body);/*
+        console.log(response)
+        expect(response).to.be.an('object');/*
+        expect(response.message).to.be.equal("hello world");*/
         // expect(response.location).to.be.an("string");
     });
 });
